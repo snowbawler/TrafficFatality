800b5d035137e37d880b86bd8a728cce2ecb10c7
snowbawler <eglaf0@gmail.com>
2023-11-09 19:16:59 -0600

    sidebar

---
+++ Website/components/sidebar.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+def sidebar():
+    with st.sidebar:
+        st.markdown(
+            "## How to use\n"
+            "1. Upload an IMG, JPEG, or PNG file📄\n"
+            "2. Scan and check Info tab for accuracy\n"
+            "3. Submit and check for fraud💬\n"
+        )
+        st.markdown("---")
+        st.markdown("# About")
+        st.markdown(
+            "📖MediGuard allows you to upload healthcare bills and claims"
+            " to identify potential error or fraud."
+        )
+        st.markdown(
+            "Powered by unsupervised machine learning"
+            " trained with healthcare billing data."
+            " MediGuard autonomously identifies suspicious patterns"
+            " and anomalies in medical bills and reports cost accuracy."
+        )
+        st.markdown("Made by [snowbawler](https://github.com/snowbawler)")
+        st.markdown("---")
