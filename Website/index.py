5ba725f2543b5b02a6111c18b3b6152e3f61a5f2
snowbawler <eglaf0@gmail.com>
2023-11-17 03:30:15 -0600

    update

--- Website/index.py
+++ Website/index.py
@@ -1,8 +1,21 @@
 import streamlit as st
 import pandas as pd
 from components.sidebar import sidebar
-from components.modules import visuals
-from components.modules import data
+from components.modules import visuals, data, modeling, modeling2, ethics
+from components.modeling import modelingCode, modelingCode2
+
+
+
+import numpy as np
+import matplotlib.pylab as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.set_page_config(
     page_title="Traffic Fatalities", 
@@ -26,4 +39,27 @@ data()
 
 #Explore Analysis
 visuals()
+
+#Model
+modeling()
+modelrun = st.button("Run Model Code")
+result = ''
+fig = ''
+if modelrun:
+    result, fig = modelingCode()
+    st.code(result)
+    plot1 = st.pyplot(fig)
+    
+modeling2()
+
+result2 = ''
+fig2 = ''
+modelrun2 = st.button("Run Model Code for Speeding")
+if modelrun2:
+    result2, fig2 = modelingCode2()
+    st.code(result2)
+    plot2 = st.pyplot(fig2)
+
+#Ethics
+ethics()
 
