632d61bb3bd13eab80655474e9e10f3f7c0973b2
snowbawler <eglaf0@gmail.com>
2023-11-18 21:12:50 -0600

    s

--- streamlit-generate-html.py
+++ streamlit-generate-html.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 # Import your main Streamlit app
 from Website import index
-from index import main  # Replace with your actual Streamlit app filename
+# Replace with your actual Streamlit app filename
 
 st.set_page_config(layout="wide")
 
 # Run the Streamlit app and render to HTML
 with st.script_request_queue():
-    main()
+    index.main()
